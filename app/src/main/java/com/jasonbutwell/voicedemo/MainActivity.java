3add5273e1821f47102627d80980d4e626e877e5
Jason Butwell <jasonbutwell@gmail.com>
2016-10-19 15:51:53 +0100

    Created a button to call the speech intent so the app is reusable. Also fixed the layout XML for both the round and square device configurations.

--- app/src/main/java/com/jasonbutwell/voicedemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/voicedemo/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -12,21 +14,24 @@ import java.util.List;
 public class MainActivity extends Activity {
 
     private TextView mTextView;
+    private Button talkButton;
 
     private final int SpeechIntentRequestCode = 0;
+
+    private void getVoice() {
+
+        // Create an intent for the speech recognizer
+
+        Intent i  = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);   // setup data for the intent
+        startActivityForResult( i, SpeechIntentRequestCode );                                            // call the intent
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-        // Address the text view to display the result
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Create an intent for the speech recognizer
-        Intent i  = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);   // setup data for the intent
-        startActivityForResult( i, SpeechIntentRequestCode);    // call the intent
 
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
@@ -35,8 +40,21 @@ public class MainActivity extends Activity {
 
                 // main part of OnCreate()
 
-            }
-        });
+                // Address the text view to display the result
+
+                mTextView = (TextView) findViewById(R.id.text);
+                talkButton = (Button) findViewById(R.id.button2);
+
+                // set up a click listener to call the intent to get the voice on click
+
+                talkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        getVoice();
+                    }
+                });
+                }
+            });
     }
 
     // Called to receive the result of the Intent
